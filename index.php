6f97d8f1a380ebde3bb26f492bea5ec8b8e99813
Seph <seph.dinh@gmail.com>
2013-03-25 06:10:47 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>ZigZag Career</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')</script>
+        <![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <header id="main">
+			<a href="/"><div id="logo"></div></a>
+			<div id="social-bar">
+				<a href="#"><img src="./imgs/fb-btn_19x19.jpg" alt="facebook" width="19" height="19" /></a>
+				<a href="#"><img src="./imgs/tt-btn_19x19.jpg" alt="twitter" width="19" height="19" /></a>
+			</div>
+			<nav id="main">
+				<ul>
+					<li><a href="#">Giới thiệu aaa</a></li>
+					<li><a href="#">Dịch vụ viết bbb</a></li>
+					<li><a href="#">Tư vấn ccc</a></li>
+					<li><a href="#">FAQs ddd</a></li>
+					<li><a href="#">Tuyển dụng eee</a></li>
+					<li><a href="#">Liên hệ fff</a></li>
+				</ul>
+			</nav>
+		</header>
+		
+		<div id="slider">
+			
+		</div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!--<script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>-->
+    </body>
+</html>
---
+++ index_old.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<html>
+	<head>
+		<title>ZigZag Career</title>
+		<link type="text/css" rel="stylesheet" href="./styles/main.css" />
+	</head>
+	<body>
+		Hello, World.
+	</body>
+</html>
