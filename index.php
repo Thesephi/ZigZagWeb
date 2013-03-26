9fbba6496bf8aa12ab6968e261c7b4a8d3fe02ac
Seph <seph.dinh@gmail.com>
2013-03-26 22:55:01 +0700

    Updated to the 1st article item (1st slide)

--- index.php
+++ index.php
@@ -31,19 +31,38 @@
 			</div>
 			<nav id="main">
 				<ul>
-					<li><a href="#">Giới thiệu aaa</a></li>
-					<li><a href="#">Dịch vụ viết bbb</a></li>
-					<li><a href="#">Tư vấn ccc</a></li>
-					<li><a href="#">FAQs ddd</a></li>
-					<li><a href="#">Tuyển dụng eee</a></li>
-					<li><a href="#">Liên hệ fff</a></li>
+					<li><a href="#">Giới thiệu</a></li>
+					<li><a href="#">Dịch vụ viết</a></li>
+					<li><a href="#">Tư vấn</a></li>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Tuyển dụng</a></li>
+					<li><a href="#">Liên hệ</a></li>
 				</ul>
 			</nav>
 		</header>
 		
-		<div id="slider">
-			
-		</div>
+		<div id="slider"></div>
+		<div id="slider-ctrl"></div>
+		
+		<article id="art0" class="clearfix">
+			<section class="sect-left">
+				<h1>Our Service</h1>
+				<p>ZigZag Career giúp khách hàng thành công trong học tập và làm việc, đồng thời xây dựng thương hiệu cá nhân đẳng cấp và chuyên nghiệp qua các dịch vụ: <strong>Viết Hồ Sơ Năng Lực, Xây dựng thương hiệu cá nhân, Tư vấn và lên kế hoạch học tập, du học và nghề nghiệp và Chiến lược tìm việc để có được công việc mơ ước.</strong> Dịch vụ của chúng tôi là tất cả những gì khách hàng cần để xây dựng một thương hiệu cá nhân thật sự chuyên nghiệp và thành công.</p>
+			</section>
+			<section class="sect-right">
+				<h1>Why's bear?</h1>
+				<p>Gấu được chúng tôi chọn là người hướng dẫn cho khách hàng khi tham quan website. Câu chuyện bắt nguồn từ biệt danh của người sáng lập nên công ty, chúng tôi quyết định linh vật là Gấu để viết câu chuyện về mình. <strong>Zigzag đã chọn những điều mình thích để làm nên bộ hồ sơ riêng, và sẽ chọn những điều bạn thích để xây dựng nên bộ hồ sơ của chính các bạn.</strong></p>
+			</section>
+		</article>
+		
+		<div class="sect-nav-arr"></div>
+		
+		<article id="art1">
+			<p class="art-child">Sự nghiệp có hình zigzag đang là xu hướng mới. Nhà tuyển dụng dần đánh giá cao các ứng viên linh hoạt, chủ động trong nấc thang sự nghệp. Và gọi họ là: <strong>Zig-Zagger</strong>.</p>
+			<p class="art-child">Hãy xây dựng uy tín cho bản thân và biến mỗi lần bạn zigzag công việc thành một điểm mạnh trong hồ sơ năng lực bằng những gói dịch vụ của ZigZag Career.</p>
+		</article>
+		
+		<div class="sect-nav-arr"></div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
