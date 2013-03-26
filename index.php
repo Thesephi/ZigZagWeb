e48632eee173c7aa745b71b4ded988f58552ab02
Seph <seph.dinh@gmail.com>
2013-03-27 04:05:21 +0700

    Added all article items (all slides)

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <header id="main">
+        <header id="main-header">
 			<a href="/"><div id="logo"></div></a>
 			<div id="social-bar">
 				<a href="#"><img src="./imgs/fb-btn_19x19.jpg" alt="facebook" width="19" height="19" /></a>
 				<a href="#"><img src="./imgs/tt-btn_19x19.jpg" alt="twitter" width="19" height="19" /></a>
 			</div>
-			<nav id="main">
+			<nav id="main-nav">
 				<ul>
 					<li><a href="#">Giới thiệu</a></li>
 					<li><a href="#">Dịch vụ viết</a></li>
@@ -57,12 +57,63 @@
 		
 		<div class="sect-nav-arr"></div>
 		
-		<article id="art1">
-			<p class="art-child">Sự nghiệp có hình zigzag đang là xu hướng mới. Nhà tuyển dụng dần đánh giá cao các ứng viên linh hoạt, chủ động trong nấc thang sự nghệp. Và gọi họ là: <strong>Zig-Zagger</strong>.</p>
-			<p class="art-child">Hãy xây dựng uy tín cho bản thân và biến mỗi lần bạn zigzag công việc thành một điểm mạnh trong hồ sơ năng lực bằng những gói dịch vụ của ZigZag Career.</p>
+		<article class="story" id="art1">
+		<div>
+			<p>Sự nghiệp có hình zigzag đang là xu hướng mới. Nhà tuyển dụng dần đánh giá cao các ứng viên linh hoạt, chủ động trong nấc thang sự nghệp. Và gọi họ là: <strong>Zig-Zagger</strong>.</p>
+			<div class="illus" id="illus1"></div>
+			<p>Hãy xây dựng uy tín cho bản thân và biến mỗi lần bạn zigzag công việc thành một điểm mạnh trong hồ sơ năng lực bằng những gói dịch vụ của ZigZag Career.</p>
+		</div>
+		</article>
+		<div class="sect-nav-arr"></div>
+		
+		<article class="story" id="art2">
+		<div>
+			<h1>Hồ sơ năng lực là gì ?</h1>
+			<p>Có bao giờ bạn thắc mắc tại sao mình không có được nhiều cơ hội tốt như người khác ? Bạn có năng lực nhưng chưa biết cách thể hiện ? <strong>Hãy bắt đầu với một bộ hồ sơ năng lực !</strong></p>
+			<div class="illus" id="illus2"></div>
+			<p><strong>Hồ sơ được xem là công cụ hiệu quả nhất nhằm marketing bản thân,</strong> thể hiện được thế mạnh và năng lực, phần thiết kế cần sự sáng tạo và khác biệt.</p>
+		</div>
+		</article>
+		<div class="sect-nav-arr"></div>
+		
+		<article class="story" id="art3">
+		<div>
+			<h1>Tại sao nên chọn Zigzag Career ?</h1>
+			<p>Tại ZigZag Career, chúng tôi tin rằng Một Hồ Sơ có thể mang đến cho bạn Triệu Cơ Hội để khẳng định năng lực và tạo thương hiệu cá nhân của mình.</p>
+			<div class="illus" id="illus3">
+				<div class="illus3-child" id="illus31">
+					<h2>Luôn luôn<br /><span class="strong-white">Sáng tạo</span></h2>
+					<p>từ viết nội dung, thiết kế đến ý tưởng chủ đạo toàn bộ hồ sơ năng lực. Chúng tôi luôn đề cao tính sáng tạo</p>
+				</div>
+				<div class="illus3-child" id="illus32">
+					<h2><span class="strong-red">Khách hàng</span><br />là trung tâm</h2>
+					<p>Chúng tôi viết và thiết kế để đáp ứng nhu cầu thực tiễn của khách hàng.</p>
+				</div>
+				<div class="illus3-child" id="illus33">
+					<h2>Hướng đến<br /><span class="strong-red">Kết quả</span></h2>
+					<p>Chúng tôi làm việc với phương châm đạt được kết quả và được khách hàng ghi nhận cho điều này.</p>
+				</div>
+				<div class="illus3-child" id="illus34">
+					<h2>Chi phí<br /><span class="strong-red">Hợp lý</span></h2>
+					<p>Khách hàng nhận được sản phẩm dịch vụ chất lượng từ một đội ngũ làm việc năng lực cao với một mức giá mà bạn hoàn toàn có thể chi trả.</p>
+				</div>
+			</div>
+		</div>
+		</article>
+		<div class="sect-nav-arr"></div>
+		
+		<article class="story" id="art4">
+		<div>
+			<p><strong>Chọn nghề nghiệp dưới đây</strong><br />để bắt đầu một hồ sơ năng lực thể hiện rõ cá tính của bạn</p>
+			<div class="illus" id="illus4"></div>
+		</div>
 		</article>
 		
-		<div class="sect-nav-arr"></div>
+		<aside id="main-aside">
+			<p><span style="font-weight:bold;">Lưu ý !!!</span><br />Ai đặt cái gì cũng viết hết đó nhoaaaaaaaaaaaa ^^ không tin gọi thử coi</p>
+		</aside>
+		
+		<footer id="main-footer"></footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+/* ZigZag Career main.js
+ * Written by Seph
+ */
+ 
+$(function() {
+	
+	// deprecated
+	//adjustStories();
+	
+	/*$(window).bind('scroll', function() {
+		console.log($(window).scrollTop());
+	});*/
+	
+	$('div.sect-nav-arr').each(function(index) {
+		$(this).bind('click', function() {
+			$("html, body").animate(
+				{ scrollTop: ($(window).scrollTop() + $(window).height()) + "px" },
+				{
+					complete : function(){
+						console.log('this alert will popup twice');
+					}
+				}
+			);
+		});
+	});
+	
+});
 
+/*
+ * deprecated but we'll keep it for the sake of reference.
+ * This function assumes the article.story elements have 700px height each
+ * and the direct child 'div' of every article.story element has to be 'vertically centered' upon page loading
+ */
+function adjustStories() {
+	$('article.story').each(function(index) {
+		var theChild = $(this).children('div')[0];
+		var theChildHeight = $(theChild).height();
+		theChild.style.position = 'relative';
+		theChild.style.top = ($(this).height()/2 - theChildHeight/2) + 'px';
+	});
+}
