6a3dc24f02d010fffdefa3294a470a646823b554
Seph <seph.dinh@gmail.com>
2013-04-01 10:11:35 +0700

    First version of a complete 1-page website

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>ZigZag Career</title>
-        <meta name="description" content="">
+        <meta name="description" content="resume writing, cv, proofreading, study abroad, consultations, company profile, personal profile, job application, personal branding">
         <meta name="viewport" content="width=device-width">
 
         <link rel="stylesheet" href="css/normalize.min.css">
@@ -26,22 +26,24 @@
         <header id="main-header">
 			<a href="/"><div id="logo"></div></a>
 			<div id="social-bar">
-				<a href="#"><img src="./imgs/fb-btn_19x19.jpg" alt="facebook" width="19" height="19" /></a>
-				<a href="#"><img src="./imgs/tt-btn_19x19.jpg" alt="twitter" width="19" height="19" /></a>
+				<a href="http://www.facebook.com/zigzagcareer" target="_blank" title="Find us on facebook" onclick="ga('send', 'event', 'User Behaviour', 'Visit Social Page', 'Facebook')"><div id="fb-btn"></div></a>
+				<a href="http://www.twitter.com/zigzagcareer" target="_blank" title="Follow us @ZigZagCareer" onclick="ga('send', 'event', 'User Behaviour', 'Visit Social Page', 'Twitter')"><div id="tt-btn"></div></a>
 			</div>
 			<nav id="main-nav">
 				<ul>
 					<li><a href="#">Giới thiệu</a></li>
-					<li><a href="#">Dịch vụ viết</a></li>
-					<li><a href="#">Tư vấn</a></li>
-					<li><a href="#">FAQs</a></li>
-					<li><a href="#">Tuyển dụng</a></li>
-					<li><a href="#">Liên hệ</a></li>
+					<li><a href="#" class="tooltip" data-tip="coming soon!">Dịch vụ viết</a></li>
+					<li><a href="#" class="tooltip" data-tip="coming soon!">Tư vấn</a></li>
+					<li><a href="#" class="tooltip" data-tip="coming soon!">FAQs</a></li>
+					<li><a href="#" class="tooltip" data-tip="coming soon!">Tuyển dụng</a></li>
+					<li><a href="#" onclick="gotoBot();ga('send', 'event', 'User Behaviour', 'Click', 'Contact navigation button')">Liên hệ</a></li>
 				</ul>
 			</nav>
 		</header>
 		
-		<div id="slider"></div>
+		<div id="slider-out">
+			<div id="slider"></div>
+		</div>
 		<div id="slider-ctrl"></div>
 		
 		<article id="art0" class="clearfix">
@@ -55,32 +57,37 @@
 			</section>
 		</article>
 		
-		<div class="sect-nav-arr"></div>
+		<article>
+			<div class="sect-nav-arr"></div>
+		</article>
 		
 		<article class="story" id="art1">
 		<div>
 			<p>Sự nghiệp có hình zigzag đang là xu hướng mới. Nhà tuyển dụng dần đánh giá cao các ứng viên linh hoạt, chủ động trong nấc thang sự nghệp. Và gọi họ là: <strong>Zig-Zagger</strong>.</p>
-			<div class="illus" id="illus1"></div>
+			<div class="illus" id="illus1" data-dim-ratio="0.41"></div>
 			<p>Hãy xây dựng uy tín cho bản thân và biến mỗi lần bạn zigzag công việc thành một điểm mạnh trong hồ sơ năng lực bằng những gói dịch vụ của ZigZag Career.</p>
+			<div class="sect-nav-arr"></div>
 		</div>
 		</article>
-		<div class="sect-nav-arr"></div>
+		
 		
 		<article class="story" id="art2">
 		<div>
 			<h1>Hồ sơ năng lực là gì ?</h1>
 			<p>Có bao giờ bạn thắc mắc tại sao mình không có được nhiều cơ hội tốt như người khác ? Bạn có năng lực nhưng chưa biết cách thể hiện ? <strong>Hãy bắt đầu với một bộ hồ sơ năng lực !</strong></p>
-			<div class="illus" id="illus2"></div>
+			<div class="illus" id="illus2" data-dim-ratio="0.41"></div>
 			<p><strong>Hồ sơ được xem là công cụ hiệu quả nhất nhằm marketing bản thân,</strong> thể hiện được thế mạnh và năng lực, phần thiết kế cần sự sáng tạo và khác biệt.</p>
+			<div class="sect-nav-arr"></div>
 		</div>
 		</article>
-		<div class="sect-nav-arr"></div>
+		
 		
 		<article class="story" id="art3">
 		<div>
 			<h1>Tại sao nên chọn Zigzag Career ?</h1>
 			<p>Tại ZigZag Career, chúng tôi tin rằng Một Hồ Sơ có thể mang đến cho bạn Triệu Cơ Hội để khẳng định năng lực và tạo thương hiệu cá nhân của mình.</p>
-			<div class="illus" id="illus3">
+			<div class="illus clearfix" id="illus3" data-smart-height="true" data-dim-ratio="0.41">
+			
 				<div class="illus3-child" id="illus31">
 					<h2>Luôn luôn<br /><span class="strong-white">Sáng tạo</span></h2>
 					<p>từ viết nội dung, thiết kế đến ý tưởng chủ đạo toàn bộ hồ sơ năng lực. Chúng tôi luôn đề cao tính sáng tạo</p>
@@ -97,35 +104,65 @@
 					<h2>Chi phí<br /><span class="strong-red">Hợp lý</span></h2>
 					<p>Khách hàng nhận được sản phẩm dịch vụ chất lượng từ một đội ngũ làm việc năng lực cao với một mức giá mà bạn hoàn toàn có thể chi trả.</p>
 				</div>
+			
 			</div>
+			<div class="sect-nav-arr"></div>
 		</div>
 		</article>
-		<div class="sect-nav-arr"></div>
+		
 		
 		<article class="story" id="art4">
 		<div>
 			<p><strong>Chọn nghề nghiệp dưới đây</strong><br />để bắt đầu một hồ sơ năng lực thể hiện rõ cá tính của bạn</p>
-			<div class="illus" id="illus4"></div>
+			<div class="illus" id="illus4" data-smart-height="true" data-dim-ratio="0.61">
+				<!--<canvas id="art4c"></canvas>-->
+				<div class="clearfix" id="illus4comps">
+					<div id="illus4comps1345">
+						<a href="http://zigzagcareer.com/downloads/zigzagcareer-leaflet-sv-studyabroad.pdf" target="_blank" onclick="ga('send', 'event', 'User Behaviour', 'Download', this.href)"><div class="illus4-comp" id="comp1"><div>Du Học</div></div></a>
+						<a href="http://zigzagcareer.com/downloads/zigzagcareer-leaflet-ns.pdf" target="_blank" onclick="ga('send', 'event', 'User Behaviour', 'Download', this.href)"><div class="illus4-comp" id="comp3"><div>Nghệ Sĩ</div></div></a>
+						<a href="http://zigzagcareer.com/downloads/zigzagcareer-leaflet-sv-resume.pdf" target="_blank" onclick="ga('send', 'event', 'User Behaviour', 'Download', this.href)"><div class="illus4-comp" id="comp4"><div>Sinh Viên</div></div></a>
+						<a href="http://zigzagcareer.com/downloads/zigzagcareer-leaflet-cv.pdf" target="_blank" onclick="ga('send', 'event', 'User Behaviour', 'Download', this.href)"><div class="illus4-comp" id="comp5"><div>Chuyên Viên</div></div></a>
+					</div>
+					<a href="http://zigzagcareer.com/downloads/zigzagcareer-leaflet-dn.pdf" target="_blank" onclick="ga('send', 'event', 'User Behaviour', 'Download', this.href)"><div class="illus4-comp" id="comp2"><div>Doanh Nhân</div></div></a>
+				</div>
+			</div>
 		</div>
 		</article>
 		
-		<aside id="main-aside">
+		<!--<aside id="main-aside">
 			<p><span style="font-weight:bold;">Lưu ý !!!</span><br />Ai đặt cái gì cũng viết hết đó nhoaaaaaaaaaaaa ^^ không tin gọi thử coi</p>
-		</aside>
+		</aside>-->
 		
-		<footer id="main-footer"></footer>
+		<footer id="main-footer">
+			<div>
+				<p>&copy; 2012 - 2013 ZigZag Career | Business inquiries: <a href="mailto:info@zigzagcareer.com" onclick="ga('send', 'event', 'User Behaviour', 'Click', 'Send email from footer link')">info@zigzagcareer.com</a></p>
+				<!--<p class="vcard">
+					<a class="fn org url" href="http://zigzagcareer.com" title="ZigZag Career Website">
+						<span class="organization-name">ZigZag Career</span>
+					</a>
+					<span class="street-address">134 N St. Mỹ Giang 2, Phú Mỹ Hưng</span>, <span class="locality">Ho Chi Minh city</span>,
+					<b class="country">Vietnam</b>
+					<span class="tel">Telephone: <span class="value">090-000-000</span></span>
+				</p>-->
+			</div>
+		</footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/jquery.mousewheel.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
+		
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        <!--<script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>-->
+		  ga('create', 'UA-23348048-8', 'zigzagcareer.com');
+		  ga('send', 'pageview');
+
+		</script>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -2,22 +2,84 @@
  * Written by Seph
  */
  
+ var _isScrolling = false;
+ var _scrollCompleteCalled = false;
+ var _storyPos = [0];
+ var _curStoryPos = 0;
+ 
 $(function() {
-	
-	// deprecated
-	//adjustStories();
 	
 	/*$(window).bind('scroll', function() {
 		console.log($(window).scrollTop());
 	});*/
 	
+	// take care of the canvas for story4
+	//canvasCtrl.init();
+	
+	adjustStories();
+	
+	$(window).bind('resize', function() {
+		adjustStories();
+	});
+	
+	$("body").mousewheel(function(event, delta) {
+		if(!_isScrolling) {
+			
+			var toPos = (delta < 0)? _curStoryPos+1 : _curStoryPos-1;
+			if(toPos < 0) {
+				_curStoryPos = 0;
+				return;
+			} else if(toPos > _storyPos.length-1) {
+				_curStoryPos = _storyPos.length-1;
+				return;
+			}
+			
+			_isScrolling = true;
+			_scrollCompleteCalled = false;
+			
+			$("html, body").animate(
+				{ scrollTop: _storyPos[toPos] + "px" },
+				{
+					complete: function() {
+						if(!_scrollCompleteCalled) {
+							_scrollCompleteCalled = true;
+							_curStoryPos = toPos;
+							setTimeout(validateScrl, 500);
+						}
+					}
+				}
+			);
+		}
+		event.preventDefault();
+	});
+	
 	$('div.sect-nav-arr').each(function(index) {
 		$(this).bind('click', function() {
+			
+			//var arrAnchor = $(this).offset().top + $(this).height();
+			var slideAmt;
+			var wholeSlideAnchor = $(window).scrollTop() + $(window).height();
+			var article = $(this).closest('article');
+			var nextSlideAnchor = article.next().children().offset().top;
+			if($(article).next().height() > $(window).height()) {
+				console.log('nextSlideAnchor');
+				slideAmt = nextSlideAnchor;
+			} else {
+				slideAmt = wholeSlideAnchor;
+			}
+			
+			_isScrolling = true;
+			_scrollCompleteCalled = false;
+			
 			$("html, body").animate(
-				{ scrollTop: ($(window).scrollTop() + $(window).height()) + "px" },
+				{ scrollTop: slideAmt + "px" },
 				{
-					complete : function(){
-						console.log('this alert will popup twice');
+					complete: function() {
+						if(!_scrollCompleteCalled) {
+							_scrollCompleteCalled = true;
+							_curStoryPos = index+1;
+							setTimeout(validateScrl, 500);
+						}
 					}
 				}
 			);
@@ -26,16 +88,113 @@ $(function() {
 	
 });
 
-/*
- * deprecated but we'll keep it for the sake of reference.
- * This function assumes the article.story elements have 700px height each
- * and the direct child 'div' of every article.story element has to be 'vertically centered' upon page loading
- */
+function validateScrl() {
+	_isScrolling = false;
+}
+
 function adjustStories() {
+
+	// per-element specific adjustments BEFORE general adjustments
+	// for div#illus3, we want its smartHeight to be "true" when window width < 480 and false otherwise
+	if($(window).width() >= 768) {
+		$('div#illus3').data('smartHeight', false);
+	} else {
+		$('div#illus3').data('smartHeight', true);
+	}
+	
+	_storyPos = [0];
+
+	// general adjustments
 	$('article.story').each(function(index) {
+		
 		var theChild = $(this).children('div')[0];
+		
+		var theIllusDiv = $(theChild).children('div.illus')[0];
+		var theIllusDivCurWidth = $(theIllusDiv).width();
+		if($(theIllusDiv).data('smartHeight')) {
+			$(theIllusDiv).css('height', 'auto'); // which means the div will base its height on its current content
+		} else {
+			var theRatio = $(theIllusDiv).data('dimRatio');
+			if(!theRatio) {
+				theRatio = 550/1330;
+			}
+			$(theIllusDiv).height(Math.round(theIllusDivCurWidth * theRatio));
+		}
+		
 		var theChildHeight = $(theChild).height();
+		
+		if($(window).height() > theChildHeight) {
+			$(this).height($(window).height());
+		} else {
+			$(this).css('height', '');
+		}
+		
 		theChild.style.position = 'relative';
-		theChild.style.top = ($(this).height()/2 - theChildHeight/2) + 'px';
+		theChild.style.top = Math.round($(this).height()/2 - $(theChild).height()/2) + 'px';
+		
+		_storyPos.push($(this).offset().top);
 	});
+	
+	// specific adjustments AFTER general adjustments
+	if(canvasCtrl.canvas) {
+		var canvas = canvasCtrl.canvas;
+		var containerWid = $('div#illus4').width();
+		var containerHei = $('div#illus4').height();
+		var imgWid = canvasCtrl.onLayerWid;
+		var imgHei = canvasCtrl.onLayerHei;
+		canvas.width = containerWid;
+		canvas.height = containerHei;
+		var imgX = containerWid/2 - imgWid/2;
+		var imgY = containerHei/2 - imgHei/2;
+		canvasCtrl.ctx.drawImage(canvasCtrl.onLayer, 0, 0, containerWid, containerHei);
+	}
+}
+
+function gotoBot() {
+	_isScrolling = true;
+	_scrollCompleteCalled = false;
+	$("html, body").animate(
+		{ scrollTop: $(document).height() },
+		{
+			complete: function() {
+				if(!_scrollCompleteCalled) {
+					_scrollCompleteCalled = true;
+					_curStoryPos = _storyPos.length-1;
+					setTimeout(validateScrl, 500);
+				}
+			}
+		},
+		500
+	);
+}
+
+var canvasCtrl = {
+	"canvas" : null,
+	"ctx" : null,
+	"onLayer" : null,
+	"onLayerWid" : 0,
+	"onLayerHei" : 0,
+	"init" : function() {
+		this.canvas = document.getElementById('art4c');
+		this.ctx = this.canvas.getContext('2d');
+		this.onLayer = document.createElement('img');
+		this.onLayer.data = {parent: this};
+		this.onLayer.src = '../imgs/home_art/04_1060x660.png';
+		this.onLayer.id = 'onLayer';
+		this.onLayer.onload = function() {
+			
+			var canvasCtrl = this.data.parent;
+			canvasCtrl.onLayerWid = $(this).width();
+			canvasCtrl.onLayerHei = $(this).height();
+			
+			var canvas = this.data.parent.canvas;
+			var ctx = this.data.parent.ctx;
+			//ctx.fillRect(0,0,780,260);
+			
+			ctx.drawImage(this, 0, 0, $('div#illus4').width(), $('div#illus4').height());
+		}
+	},
+	"mask1" : function() {
+		console.log(this.ctx);
+	}
 }
